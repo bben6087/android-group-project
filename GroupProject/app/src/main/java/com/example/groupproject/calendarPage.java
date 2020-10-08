dd0ee25abe1c5f598206175af8da5e0768ba3fc1
Nolan Harless <nolanharless@gmail.com>
2020-10-08 16:47:02 -0500

    Added java classes for scheduler and messenger pages
    Added java classes for scheduler and messenger pages

---
+++ GroupProject/app/src/main/java/com/example/groupproject/calendarPage.java
@@ -0,0 +1,14 @@
+package com.example.groupproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class calendarPage extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_page);
+    }
+}
---
+++ GroupProject/app/src/main/java/com/example/groupproject/messengerPage.java
@@ -0,0 +1,13 @@
+package com.example.groupproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class messengerPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.messenger_page);
+    }
+}
