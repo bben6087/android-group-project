574167ee2b0cbc31ce6f08d43f80ebda73a33d82
Blake Bennett <s532542@nwmissouri.edu>
2020-11-17 19:17:23 -0600

    I redid all of my Parse logic for the sign up. It was causing errors so I did it a new way. All errors fixed and validation works as well. Now it adds to the database flawlessly. - Blake

--- GroupProject/app/src/main/java/com/example/groupproject/SignUp.java
+++ GroupProject/app/src/main/java/com/example/groupproject/SignUp.java
@@ -1,5 +1,7 @@
 package com.example.groupproject;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,10 +9,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
 
 public class SignUp extends AppCompatActivity {
@@ -19,19 +23,29 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                // if defined
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        );
+
+        // Save the current Installation to Back4App
+        ParseInstallation.getCurrentInstallation().saveInBackground();
 
     }
 
-    public void createAccount(View v){
+    public void createAccount(View v) {
         EditText snumET = findViewById(R.id.createSNumET);
         String snumStr = snumET.getText().toString();
         EditText passwordET = findViewById(R.id.createPasswordET);
         String passStr = passwordET.getText().toString();
-        if(validateSNum(snumStr)== true && !snumStr.equals("") && !passStr.equals("")){
-            ParseUser user = new ParseUser();
-            user.setUsername(snumStr);
-            user.setPassword(passStr);
-            user.signUpInBackground(new SignUpCallback() {
+        if (validateSNum(snumStr) == true && !snumStr.equals("") && !passStr.equals("")) {
+            ParseObject login = new ParseObject("Login");
+            login.put("username", snumStr);
+            login.put("password", passStr);
+            login.saveInBackground(new SaveCallback() {
                 @Override
                 public void done(ParseException e) {
                     Log.d("Parse", "Reg: " + e);
@@ -50,11 +64,11 @@ public class SignUp extends AppCompatActivity {
 
     public static boolean validateSNum (String txt){
         for(char c: txt.toCharArray()){
-            if(!(Character.isDigit(c) || c == 's')){
-                return false;
+            if(Character.isDigit(c) || c == 's'){
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     public void goToHomePage(View v){
